20688699e1f0291fa1b9679e3f8cece0416f1134
masuyama13 <masterxwebm13@gmail.com>
2020-12-12 23:59:47 +0900

    Fruitモデル・ビュー・コントローラ・ルーティング作成
    bin/rails g scaffold fruit name:string description:text
    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_10_163749) do
+ActiveRecord::Schema.define(version: 2020_12_12_145622) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "fruits", force: :cascade do |t|
+    t.string "name"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
