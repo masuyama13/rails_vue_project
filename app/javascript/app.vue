24442d699d5e5a0fab5acc23c2dabf73ff76a118
masuyama13 <masterxwebm13@gmail.com>
2020-12-13 15:25:24 +0900

    VueでFruitsを表示

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,6 +1,25 @@
 <template>
   <div id="app">
     <p>{{ message }}</p>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th></th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="fruit in fruits">
+          <td>{{ fruit.name }}</td>
+          <td>{{ fruit.description }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <p>----- Vue ここまで -----</p>
   </div>
 </template>
 
@@ -8,15 +27,33 @@
 export default {
   data() {
     return {
-      message: "Hello Vue!"
+      message: "Hello Vue!",
+      fruits: []
     }
+  },
+  created() {
+    fetch(`/fruits.json`, {
+      method: 'GET',
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+      credentials: 'same-origin',
+    })
+      .then(response => {
+        return response.json()
+      })
+      .then(json => {
+        json.forEach(c => { this.fruits.push(c) });
+      })
+      .catch(error => {
+        console.warn('Failed to parsing', error)
+      })
   }
 }
 </script>
 
 <style scoped>
 p {
-  font-size: 2em;
   text-align: center;
 }
 </style>
